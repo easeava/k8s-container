240125857c583474bea31a43617e986ffedc40c1
easeava <tthd@163.com>
2020-06-30 10:21:41 +0800

    ansible

--- roles/keepalived_haproxy/files/keepalived_haproxy.sh
+++ roles/keepalived_haproxy/files/keepalived_haproxy.sh
@@ -4,12 +4,18 @@ set -e
 
 WORK_PATH=/home/ease/work/install
 
-if [[ $1 && $1 == 'master-01' ]]
+cd $WORK_PATH
+
+if [[ -d k8s-compose ]]
 then
-  sed -i 's/BACKUP/MASTER/' ${WORK_PATH}/k8s-compose/keepalived/keepalived.conf
+  cd docker-compose && docker-compose stop && cd ../ && rm -rf k8s-compose
 fi
 
-cd $WORK_PATH && rm -rf k8s-compose
 git clone -b compose https://github.com/easeava/k8s-container.git k8s-compose && cd k8s-compose
-docker-compose stop
+
+if [[ $1 && $1 == 'master-01' ]]
+then
+  sed -i 's/BACKUP/MASTER/' ./keepalived/keepalived.conf
+fi
+
 docker-compose up --build -d

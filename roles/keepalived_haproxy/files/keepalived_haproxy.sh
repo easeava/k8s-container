c0bdd4cdf7f1bb9145d7a15f32adf932ed5e53db
easeava <tthd@163.com>
2020-06-30 10:40:47 +0800

    ansible

--- roles/keepalived_haproxy/files/keepalived_haproxy.sh
+++ roles/keepalived_haproxy/files/keepalived_haproxy.sh
@@ -7,9 +7,9 @@ WORK_NAME=k8s-compose
 
 cd $WORK_PATH
 
-if [[ -d $WORK_NAME ]]
+if [ -d $WORK_NAME ]
 then
-  cd $WORK_NAME && dcoker-compose stop && cd ../ && rm -rf $WORK_NAME
+  cd $WORK_NAME && docker-compose stop && cd ../ && rm -rf $WORK_NAME
 fi
 
 git clone -b compose https://github.com/easeava/k8s-container.git $WORK_NAME && cd $WORK_NAME
